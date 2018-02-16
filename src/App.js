b4a539ba946ae02549f7c8ffa696799176a1a657
David Hirst <34040303+drwhirstyburgers@users.noreply.github.com>
2018-02-16 14:37:25 -0500

    functionality added

--- src/App.js
+++ src/App.js
@@ -10,13 +10,29 @@ class App extends Component {
         { description: 'Walk the cat', isCompleted: true},
         { description: 'Throw the dishes away', isCompleted: false},
         { description: 'Buy new dishes', isCompleted: false},
-      ]
+      ],
+      newToDoDescription: ''
     };
   }
 
-  toggleComplete() {
-    console.log('toggleComplete executed');
+  handleChange(e) {
+    this.setState({ newToDoDescription: e.target.value });
   }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.newToDoDescription) {return}
+    const newToDo = { description: this.state.newToDoDescription, isCompleted: false };
+    this.setState({ todos: [...this.state.todos, newToDo], newToDoDescription: '' });
+  }
+
+  toggleComplete(index) {
+    const todos = this.state.todos.slice();
+    const todo = todos[index];
+    todo.isCompleted = todo.isCompleted ? false : true;
+    this.setState({ todos: todos });
+  }
+
 
   render() {
     return (
@@ -26,6 +42,10 @@ class App extends Component {
             <ToDo key={ index }  description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) }/>
           )}
         </ul>
+        <form onSubmit={ (e) => this.handleSubmit(e) }>
+          <input type="text" value={ this.state.newToDoDescription } onChange={ (e) => this.handleChange(e) }/>
+          <input type="submit" />
+        </form>
       </div>
     );
   }
