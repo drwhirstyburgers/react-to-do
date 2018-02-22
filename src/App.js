e2b79e32d1ba5079a87aceddb7c87452714e685e
David Hirst <34040303+drwhirstyburgers@users.noreply.github.com>
2018-02-22 12:13:46 -0500

    working on delete

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ class App extends Component {
     this.setState({ todos: todos });
   }
 
-  deleteTodo() {
-    console.log("clicked");
+  deleteTodo(index) {
+   console.log(index)
   }
 
   render() {
@@ -42,7 +42,7 @@ class App extends Component {
       <div className="App">
         <ul>
           { this.state.todos.map( (todo, index) =>
-            <ToDo key={ index }  description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) }/>
+            <ToDo key={ index }  description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } deleteTodo={ () => this.deleteTodo(index) }/>
           )}
         </ul>
         <form onSubmit={ (e) => this.handleSubmit(e) }>
